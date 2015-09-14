40c3ec447e0766fed5d9f844b1dd9d72b8d759e3
yatelu1 <yatelu1@hotmil.com>
2015-09-14 11:43:42 +0800

    add

--- js/entry.js
+++ js/entry.js
@@ -23,15 +23,16 @@ $(function () {
             $('#index-cut-04').addClass('action-bg').html("<div class='sreen'><div class='left-action'></div><div class='right-action'></div><div class='message-action'><div class='message-action-inner'></div></div></div>");
             $.ajax({
                 method: "GET",
-                url: "../mock/data.json",
-                data: {}
+                //url: "../mock/data.json",
+                url: "http://testeventapi.cosfund.com/DLD/Tran.aspx?wAction=103&wParam=name=%E4%B8%80%E6%89%8B%E6%92%B8%E5%A4%A7%E7%A5%9E_avator=1_userid=1439875688000",
+                dataType: 'json'
             }).success(function (data) {
                 if (data.ReturnCode === 0) {
                     var idx = 0;
                     changeAction(idx);
                     function changeAction(i) {
                         if (i < data.PKInfos.length) {
-                            $('.message-action-inner').html('<p>' + data.PKInfos[i].Message + '</p>');
+                            $('.message-action-inner').prepend('<p>' + data.PKInfos[i].Message + '</p>');
                             showActionImg('.left-action', splitImg(data.PKInfos[i].ShowImages[0]).img1);
                             showActionImg('.right-action', splitImg(data.PKInfos[i].ShowImages[0]).img2);
                             if (data.PKInfos[i].ShowImages[1]) {
